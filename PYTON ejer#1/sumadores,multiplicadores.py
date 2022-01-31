49799942f9873fa7077e29ebfe3afe318e69160e
pikachu21xd <201900690@est.umss.edu>
2022-01-31 17:57:39 -0400

    subir ejercicio #1 de Pyton

---
+++ PYTON ejer#1/sumadores,multiplicadores.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 31 17:22:24 2022
+
+@author: HP-2016
+"""
+#sumadores 
+sum=0
+list=(1,2,3,4,5,6,7,8,9,10)
+for i in list:
+    sum= sum + i 
+print(f'la suma total del 1 al 10 es:{sum}')
+#multiplicadores  
+mult=1
+list=(1,2,3,4,5,6,7,8,9,10)
+for i in list:
+    mult= mult * i 
+print(f'la multiplicacion total del 1 al 10 es:{mult}')    

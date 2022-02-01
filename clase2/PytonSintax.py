558d14d2ece961b43844cb68fb35bab1e38677a5
pikachu21xd <201900690@est.umss.edu>
2022-02-01 07:56:41 -0400

    ejercicio clase 2

---
+++ clase2/PytonSintax.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  1 07:21:38 2022
+
+@author: HP-2016
+"""
+
+#comentario en line
+"""
+comentarios en liena
+liena 1 
+linea 2
+"""
+#crear una variable
+NAME='JAVI'
+FULL_NAME='JAVIER DAVID PACHECO'
+print(NAME)
+print(FULL_NAME)
+#tipos de datos
+entero = 90
+print(f'el valor del entero es: {entero}')
+decimal = 6.14
+print(f'el valor del decimal es: {decimal}')
+caracter ='j'
+print(f'el valor del caracter es: {caracter}')
+cadena = 'primera cadena'
+print(f'el valor del entero es: {cadena}')
+cadena_2 = " segunda cadena"
+print(f'el valor del entero es: {cadena_2}')
+booleno = False # valores true o false 
+print(f'el valor del ecimal es: {booleno}')
+# lista
+list = [1,2,3,4]
+print(f'el valor del ecimal es: {list}')
+# diccionario
+diccionario = {'nombre' : 'javier david pacheco',"edad":21}
+nombre = diccionario [ "nombre"]
+edad = diccionario ["edad"]
+print(f'el nombre es: {nombre} y su edad {edad} ')
+ 
+# entradas estandar 
+
+telefono = input("ingrese el numero telefonico:")
+# cast de tipos 
+a = int(input("ingrese a :"))
+b = int(input("ingrese b :"))
+print(f' resultado de sumas a +b = { a+b}')
+# hasta aqui xd

82e43ad7ab3d597335f7ad1d484b3d2124fe5b02
pikachu21xd <201900690@est.umss.edu>
2022-02-04 14:17:28 -0400

    completamos la clase de avance

---
+++ clase 5/utilarchivos.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  4 12:58:19 2022
+
+@author: HP-2016
+"""
+
+#Manejar archivos
+
+numeros=open("ListaNumeros compu.txt","r")
+print(numeros.readline())
+print(numeros.readline())
+print(numeros.readline())
+
+
+# leer lineas de un archivo
+res=numeros.readlines() # restor un arary con el contenidos de archivo
+print(res)
+
+
+# anexar archivos
+
+archivo=open('lista profes.txt','a') # a = anexo
+archivo.write('nuevo docente')  # agregamos nueva linea 
+archivo.close()

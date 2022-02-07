0201dc73c0c6980d0b3c67fc526c309c2fc936d5
pikachu21xd <201900690@est.umss.edu>
2022-02-07 07:36:02 -0400

    pregunta 1 simulacro

---
+++ examen_sim_01/p1 examen_sim_01/p1 ex_sim_01.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  7 07:17:09 2022
+
+@author: HP-2016
+"""
+
+#1.Pedir una cadena al usuario y contar el numero de caracteres
+
+frase=input("ingrese la frase :")
+caracter=0
+for i in frase:
+        caracter=caracter+1
+   
+print( "el numero de caracteres es :", caracter)

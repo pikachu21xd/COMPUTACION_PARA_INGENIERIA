540ca7ae7d354ffdea8cc17c86702d56b7efa67e
pikachu21xd <201900690@est.umss.edu>
2022-02-09 08:37:00 -0400

    subimos p2

--- examen/p2/p2.py
+++ examen/p2/p2.py
@@ -5,3 +5,19 @@ Created on Wed Feb  9 07:13:53 2022
 @author: HP-2016
 """
 
+# 2.- Dada la lista [10,20,30,10,5, 1, 3, 5, 4] separar en dos listas una lista debe contener solo los pares y la segunda lista solo debe contener los impares
+
+listaDeNumeros=[10,20,30,10,5, 1, 3, 5, 4]
+lista_1=[]
+lista_2=[]
+
+for impar in listaDeNumeros:
+    if impar % 2 == 1 :
+     lista_1.append(impar)
+print("numeros impares son:",lista_1)
+   
+for par in listaDeNumeros:
+    if par % 2 == 0 :
+     lista_2.append(par)
+print("numeros pares son:",lista_2)
+      

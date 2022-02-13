93456827a484caf37d7b4098a006bf5993f70e7d
pikachu21xd <201900690@est.umss.edu>
2022-02-12 22:52:15 -0400

    hola mundo en tk

---
+++ clase 8/hello_world_gui.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 12 22:46:52 2022
+
+@author: HP-2016
+"""
+
+from tkinter import *
+root =Tk()
+a= Label(root, text="hola mundo")
+a.pack()
+root.mainloop()
